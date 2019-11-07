b2f7f72c3b96c4aa96484f6b383de6ce39f2d3fa
Mathis Hofer <hofer@puzzle.ch>
2019-11-07 10:14:36 +0100

    Use commit from version.js as version

--- src/app/sentry-error-handler.ts
+++ src/app/sentry-error-handler.ts
@@ -1,10 +1,13 @@
 import { Injectable, ErrorHandler } from '@angular/core';
 import * as Sentry from '@sentry/browser';
 
+const VERSION = (window as any).version;
+console.log('VERSION', VERSION);
+
 Sentry.init({
   dsn: 'https://7a1b1091ca034a64ace2d5a030fab531@sentry.puzzle.ch/24',
   environment: 'integration',
-  release: '1.26'
+  release: VERSION
 });
 
 @Injectable()
