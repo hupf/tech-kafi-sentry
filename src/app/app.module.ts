c7d10bbf9fcb17cda5b6d2ca4af5e7a716046303
Mathis Hofer <hofer@puzzle.ch>
2019-11-07 09:18:14 +0100

    Add Sentry error handler

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ErrorHandler } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SentryErrorHandler } from './sentry-error-handler';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  providers: [{ provide: ErrorHandler, useClass: SentryErrorHandler }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/sentry-error-handler.ts
@@ -0,0 +1,16 @@
+import { Injectable, ErrorHandler } from '@angular/core';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+  dsn: 'https://7a1b1091ca034a64ace2d5a030fab531@sentry.puzzle.ch/24',
+  environment: 'integration',
+  release: '1.26'
+});
+
+@Injectable()
+export class SentryErrorHandler implements ErrorHandler {
+  handleError(error: any): void {
+    const eventId = Sentry.captureException(error.originalError || error);
+    Sentry.showReportDialog({ eventId });
+  }
+}
